08db0394e7676f7c77258d0cae25b324cff9cadf
Yevheniy Dovbnya <ydovbnya@swivl.com>
2019-11-06 11:22:18 +0200

    Add support for diff build types

--- step.sh
+++ step.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-envman add --key APP_PROJECT_NAME
-envman add --key APP_CENTER_APP_ID
-envman add --key APP_CENTER_ACCESS_TOKEN
-envman add --key APP_CENTER_DISTRIBUTION_GROUP
-envman add --key APP_CENTER_RELEASE_NOTE
+'app_project_version_name': ${app_project_version_name}
+'app_project_version_code': ${app_project_version_code}
+'app_apk_path': ${app_apk_path}
+'app_mapping_path': ${app_mapping_path:-''}
+'app_center_app_id': ${app_center_app_id}
+'app_center_access_token': ${app_center_access_token}
+'app_center_distribution_group': ${app_center_distribution_group}
+'app_center_release_notes': ${app_center_release_notes:-''}
 
-echo "run AppCenter mapping.txt uploader..."
+if [[ -n "${app_mapping_path}" ]]; then
 
-VERSION_NAME=`grep "versionName" ${BITRISE_SOURCE_DIR}/${APP_PROJECT_NAME}/build.gradle | sed -e 's/[^.0-9]//g'`
-VERSION_CODE=`grep "versionCode" ${BITRISE_SOURCE_DIR}/${APP_PROJECT_NAME}/build.gradle | sed -e 's/[^.0-9]//g'`
+    echo "Version name: "${app_project_version_name}
+    echo "Version code: "${app_project_version_code}
 
-echo "Version name: "$VERSION_NAME
-echo "Version code: "$VERSION_CODE
-
-if [ -n "${BITRISE_MAPPING_PATH}" ]; then
 	echo "mapping.txt is found."
 	echo "run AppCenter mapping.txt uploader..."
 
-	FILENAME="mapping_"$(date +%Y%m%d_%H%M%S)"_GMT.txt"
-	cp ${BITRISE_MAPPING_PATH} ./${FILENAME}
+	FILENAME="mapping.txt"
+	cp ${app_mapping_path} ./${FILENAME}
 	sudo apt install -y jq >/dev/null
 
-	RESULT_JSON=`curl -X POST "https://api.appcenter.ms/v0.1/apps/${APP_CENTER_APP_ID}/symbol_uploads" \
+	RESULT_JSON=`curl -X POST "https://api.appcenter.ms/v0.1/apps/${app_center_app_id}/symbol_uploads" \
 	 -H "accept: application/json" \
-	 -H "X-API-Token: ${APP_CENTER_ACCESS_TOKEN}" \
+	 -H "X-API-Token: ${app_center_access_token}" \
 	 -H "Content-Type: application/json" \
-	 -d "{ \"symbol_type\": \"AndroidProguard\", \"file_name\": \"${FILENAME}\", \"build\": \"${VERSION_CODE}\", \"version\": \"${VERSION_NAME}\"}"`
+	 -d "{ \"symbol_type\": \"AndroidProguard\", \"file_name\": \"${FILENAME}\", \"build\": \"${app_project_version_code}\", \"version\": \"${app_project_version_name}\"}"`
 
 	SYMBOL_UPLOAD_ID=`echo ${RESULT_JSON} | jq '.symbol_upload_id' -r`
 	UPLOAD_URL=`echo ${RESULT_JSON} | jq '.upload_url' -r`
@@ -40,9 +39,9 @@ if [ -n "${BITRISE_MAPPING_PATH}" ]; then
 	 -H "x-ms-date: ${DATE_NOW}" \
 	 --data-binary "@${FILENAME}"
 
-	curl -X PATCH "https://api.appcenter.ms/v0.1/apps/${APP_CENTER_APP_ID}/symbol_uploads/${SYMBOL_UPLOAD_ID}" \
+	curl -X PATCH "https://api.appcenter.ms/v0.1/apps/${app_center_app_id}/symbol_uploads/${SYMBOL_UPLOAD_ID}" \
 	-H "accept: application/json" \
-	-H "X-API-Token: ${APP_CENTER_ACCESS_TOKEN}" \
+	-H "X-API-Token: ${app_center_access_token}" \
 	-H "Content-Type: application/json" \
 	-d "{ \"status\": \"committed\"}"
 
@@ -60,14 +59,6 @@ else
   npm install -g appcenter-cli
 fi
 
-if [ -n "${BITRISE_MAPPING_PATH}" ]; then
-  echo "use signed apk"
-  APP_CENTER_APK_PATH=$BITRISE_SIGNED_APK_PATH
-else
-  echo "use unsign apk"
-  APP_CENTER_APK_PATH=$BITRISE_APK_PATH
-fi
-
-appcenter distribute release -f "${APP_CENTER_APK_PATH}" -r "${APP_CENTER_RELEASE_NOTE}" -a "${APP_CENTER_APP_ID}" --token "${APP_CENTER_ACCESS_TOKEN}" -g "${APP_CENTER_DISTRIBUTION_GROUP}"
+appcenter distribute release --token "${app_center_access_token}" --file "${app_apk_path}" --app "${app_center_app_id}" --group "${app_center_distribution_group}" --release-notes "${app_center_release_notes}"
 
 exit 0
